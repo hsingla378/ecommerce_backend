ce8d15b2fbf57e5cd7b016cee36b5ceb9cdd9cfb
hsingla378 <hsingla378@gmail.com>
2022-06-28 08:32:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express")
 const port = 8080
 const app = express()
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`server is now start working on port ${port}`)
+})
 
 app.get("/", (req, res) =>{
     res.send("The API is Working")
